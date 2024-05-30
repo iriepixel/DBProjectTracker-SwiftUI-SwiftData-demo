6ffcb9c22dfb627f449dea2f0623dc6d628a75d5
Jev IRIE PIXEL <jev@iriepixel.com>
2024-05-30 23:41:17 +0100

    Initial Commit

---
+++ DBProjectTracker/DBProjectTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBProjectTrackerApp.swift
+//  DBProjectTracker
+//
+//  Created by Jev Smolnikov on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DBProjectTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
